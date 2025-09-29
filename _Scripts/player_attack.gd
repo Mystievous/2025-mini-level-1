0ecf6fe4b691d14c754c91159b5383027a7ba148
DjRePrincess <148670490+DjRePrincess@users.noreply.github.com>
2025-09-29 15:52:12 -0700

    before i put the vertical attack

--- _Scripts/player_attack.gd
+++ _Scripts/player_attack.gd
@@ -3,20 +3,27 @@ extends RayCast2D
 @onready var ray_cast_right: RayCast2D = $"."
 @onready var ray_cast_left: RayCast2D = $"../RayCastLeft"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var horizontal_direction := Input.get_axis("move_left", "move_right")
 	var vertcal_direction := Input.get_axis("move_up", "move_down")
 	
-	#var collider_name: String = ray_cast_right.get_collider()
-	if horizontal_direction == 0 or horizontal_direction == 1:
-		if ray_cast_right.is_colliding(): 
-			if Input.is_action_just_pressed("attack"):
+	handle_right_attack(horizontal_direction)
+	handle_left_attack(horizontal_direction)
+	
+func handle_right_attack(direction):
+	if direction == 0 or direction == 1:
+		if Input.is_action_just_pressed("attack"):
+#		play animation here 
+			print("animation right attack played")
+			if ray_cast_right.is_colliding(): 
 				print("hit with right attack")
-			#			which body is the raycastright hitting 
+				
+func handle_left_attack(direction):
+	if direction == 0 or direction == -1:
+		if Input.is_action_just_pressed("attack"):
+#		play animation here 
+			print("animation left attack played")
+			if ray_cast_left.is_colliding(): 
+				print("hit with right attack")
 			
