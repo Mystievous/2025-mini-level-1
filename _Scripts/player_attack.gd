72b6d38a6b56b40d357cba74ec7cc0847e2cdaf6
DjRePrincess <148670490+DjRePrincess@users.noreply.github.com>
2025-09-29 15:38:39 -0700

    right attack working properly

--- _Scripts/player_attack.gd
+++ _Scripts/player_attack.gd
@@ -13,8 +13,10 @@ func _physics_process(delta: float) -> void:
 	var horizontal_direction := Input.get_axis("move_left", "move_right")
 	var vertcal_direction := Input.get_axis("move_up", "move_down")
 	
+	#var collider_name: String = ray_cast_right.get_collider()
 	if horizontal_direction == 0 or horizontal_direction == 1:
-		if ray_cast_right.collide_with_bodies and Input.is_action_just_pressed("attack"):
-			print(ray_cast_right.get_collider())
+		if ray_cast_right.is_colliding(): 
+			if Input.is_action_just_pressed("attack"):
+				print("hit with right attack")
 			#			which body is the raycastright hitting 
 			
