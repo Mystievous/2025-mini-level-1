fb5943927da9baa6f54c6566ae90ab8ca339d0a6
DjRePrincess <148670490+DjRePrincess@users.noreply.github.com>
2025-09-29 21:31:41 -0700

    bug
    both up and down attacks are being triggered

--- _Scripts/player_attack.gd
+++ _Scripts/player_attack.gd
@@ -2,28 +2,48 @@ extends RayCast2D
 
 @onready var ray_cast_right: RayCast2D = $"."
 @onready var ray_cast_left: RayCast2D = $"../RayCastLeft"
+@onready var ray_cast_down: RayCast2D = $"../RayCastDown"
+@onready var ray_cast_up: RayCast2D = $"../RayCastUp"
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../AnimatedSprite2D"
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var horizontal_direction := Input.get_axis("move_left", "move_right")
-	var vertcal_direction := Input.get_axis("move_up", "move_down")
+	var vertical_direction := Input.get_axis("move_down", "move_up")
+	
+	#print(vertical_direction)
 	
 	handle_right_attack(horizontal_direction)
 	handle_left_attack(horizontal_direction)
+	handle_down_attack(vertical_direction)
+	handle_up_attack(vertical_direction)
 	
 func handle_right_attack(direction):
 	if direction == 0 or direction == 1:
 		if Input.is_action_just_pressed("attack"):
-#		play animation here 
-			print("animation right attack played")
+			#animated_sprite_2d.play("Attack")
 			if ray_cast_right.is_colliding(): 
 				print("hit with right attack")
 				
 func handle_left_attack(direction):
 	if direction == 0 or direction == -1:
 		if Input.is_action_just_pressed("attack"):
-#		play animation here 
-			print("animation left attack played")
+			#animated_sprite_2d.play("Attack")
 			if ray_cast_left.is_colliding(): 
-				print("hit with right attack")
+				print("hit with left attack")
 			
+func handle_down_attack(direction):
+	if direction == 0 or direction == -1:
+		if Input.is_action_just_pressed("attack"):
+			#animated_sprite_2d.play("Attack")
+			if ray_cast_down.is_colliding(): 
+				print("hit with down attack")
+				
+func handle_up_attack(direction):
+	if direction == 0 or direction == 1:
+		if Input.is_action_just_pressed("attack"):
+			#animated_sprite_2d.play("Attack")
+			if ray_cast_down.is_colliding(): 
+				print("hit with up attack")
