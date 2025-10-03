866b05761bc840f131dcd22b5cd840457cbaab1e
ko545 <acadenaperales3860@sdsu.edu>
2025-10-02 17:10:32 -0700

    added enemy waves
    the enemy spawner can now spawn enemies in waves.
    
    new exported values are number of waves, amount of enemies in wave, and show debug visuals.
    
    show debug visual will display a sprite where the spawner is, show the spawn radius ,and will print the current wave alongside the children of the Spawner node.

--- scripts/enemyTexture.gd
+++ scripts/enemyTexture.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 
-#this is just so i can use the selfmodulate property to change the color of each enemy without manualy recoloring them
+#this is just so i can use the Self Modulate property to change the color of each enemy without manually recoloring them
 
 @export var image: Texture2D
 
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,6 +4,9 @@ var enemySceneRed   = preload("res://scenes/enemies/enemyRed.tscn")
 var enemySceneGreen = preload("res://scenes/enemies/enemyGreen.tscn")
 var enemySceneBlue  = preload("res://scenes/enemies/enemyBlue.tscn")
 
+@onready var debug_visuals: Node = $DebugVisuals
+@onready var debug_spawner_range_visual: Sprite2D = $DebugVisuals/SpawnerRange
+
 enum SpawnerTypeEnum {
 	RANDOM, ##0, randomly spawn red, green or blue
  	   RED, ##1, spawns red
@@ -13,17 +16,46 @@ enum SpawnerTypeEnum {
 
 ##Determines what monsters will spawn from the Marker2D spawner. Default: Random
 @export var spawnerType: SpawnerTypeEnum
-
 ##Determines how far enemies can spawn from the Marker2D spawner. Default: 100
 @export var spawnRadiusSize: int = 100 
+##Sets how many times enemies will reappear after they are all defeated. Default: 3
+@export var numberOfWaves: int = 3
+##Sets how many enemies are in a wave. Default: 5
+@export var amountOfEnemiesInWave: int = 5
+##Determines whether the sprite at the spawner's position, the spawn radious and debug text is shown. Default: false
+@export var showDebugVisual: bool = false
 
+#keeps track of the current wave
+var currentWave: int = 0
+
+func _ready() -> void:
+	if showDebugVisual:
+		debug_visuals.show()
+		debug_spawner_range_visual.apply_scale(Vector2(spawnRadiusSize,spawnRadiusSize))
+
+#does a new wave when the wave conditions are met
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"): #spawns a new enemy when spacebar is pressed
-		spawnEnemy()
+	waves()
+	#when spacebar is pressed: deletes all enemies in the wave so the next one can appear
+	if Input.is_action_just_pressed("ui_accept"):
+		for childrenCounter in self.get_children():
+			if (childrenCounter.get_index() > 0): #skips the first node because that one is the DebugVisual
+				self.get_child(childrenCounter.get_index()).queue_free()
+
+#do the next wave if all enemies are defeated and currentWave < numberOfWaves
+func waves() -> void:
+		if (currentWave < numberOfWaves) and (self.get_child_count() <= 1):
+			for enemiesToSpawn in amountOfEnemiesInWave:
+				spawnEnemy()
+			currentWave += 1
+			if showDebugVisual:
+				print("curent wave is ",  currentWave)
+				print(self.get_children())
+				print()
 
 func spawnEnemy() -> void:
 	var enemyInstance
-	var spawnerTypeSelection
+	var spawnerTypeSelection: int
 	
 	if spawnerType == SpawnerTypeEnum.RANDOM:
 		spawnerTypeSelection = randi_range(1, 3) #if Spawner Type is set to random, it will set spawnerTypeSelection to a random int between 1 and 3 inclusive
@@ -37,9 +69,9 @@ func spawnEnemy() -> void:
 		_:
 			#print("Default case in spawner match statement")
 			enemyInstance = enemySceneRed.instantiate()
-	add_child(enemyInstance) #adds the new enemy to the scene
+	add_child(enemyInstance) #adds the new enemy to the scene	
 
-	#modifies the enemies position to a random range determined by spawnRadiousSize
-	var xOffset = randi_range(-spawnRadiusSize, spawnRadiusSize)
-	var yOffset = randi_range(-spawnRadiusSize, spawnRadiusSize)
+	#modifies the enemies position to a random range determined by spawnRadiusSize
+	var xOffset: int = randi_range(-spawnRadiusSize, spawnRadiusSize)
+	var yOffset: int = randi_range(-spawnRadiusSize, spawnRadiusSize)
 	enemyInstance.position = Vector2i(xOffset, yOffset)
