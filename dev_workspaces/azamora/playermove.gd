159dacb1df569c2354b6417b163a185504044ca6
zoroarc20 <easy051802@gmail.com>
2025-10-03 12:14:11 -0700

    comments + additional sprites
    added explanation for some code + animation sprites for blue and red (haven't updated how to make player change animation when touching another orb)

--- dev_workspaces/azamora/playermove.gd
+++ dev_workspaces/azamora/playermove.gd
@@ -1,7 +1,7 @@
 class_name Playermove
 extends CharacterBody2D
 
-@export var speed : float = 250
+@export var speed : float = 250 #player speed
 @export var animation_tree: AnimationTree
 
 var input : Vector2
@@ -10,6 +10,7 @@ var playback : AnimationNodeStateMachinePlayback
 func _ready():
 	playback = animation_tree["parameters/playback"]
 
+#directional input
 func _physics_process(_delta: float) -> void:
 	input = Input.get_vector("left", "right", "up", "down")
 	velocity = input * speed
@@ -17,6 +18,7 @@ func _physics_process(_delta: float) -> void:
 	select_animation()
 	update_animation_parameters()
 
+#checks if WASD or arrow keys are being pressed on for animation
 func select_animation():
 	if velocity == Vector2.ZERO:
 		playback.travel("Idle")
@@ -27,6 +29,6 @@ func update_animation_parameters():
 	if input == Vector2.ZERO:
 		return
 		
+	# determines what direction to face
 	animation_tree["parameters/Idle/blend_position"] = input
 	animation_tree["parameters/Walk/blend_position"] = input
-	
